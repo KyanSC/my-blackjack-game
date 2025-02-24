39fc674855bc3b0c6e0da9353b0f669026a5dee0
KyanSC <kyansantiagocalling@gmail.com>
2025-02-24 10:31:12 -0800

    Added comment to backend.py

--- backend.py
+++ backend.py
@@ -22,6 +22,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+#Ignore this
+
 #Card Deck
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
